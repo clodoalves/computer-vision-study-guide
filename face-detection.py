e42eb925a12b677f30e5f11ccbd891a5dda241f6
José Clodoalves da Silva Júnior <jcsj3@dmz.org.br>
2023-04-19 18:24:15 -0300

    Add Gray scale image implementation

--- face-detection.py
+++ face-detection.py
@@ -1,1 +1,17 @@
 import cv2;
+
+image = cv2.imread("content/images/people1.jpg")
+
+print(image.shape)
+
+image = cv2.resize(image, (800, 600))
+
+grayScaleImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("sample", grayScaleImage)
+
+print(grayScaleImage.shape)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
