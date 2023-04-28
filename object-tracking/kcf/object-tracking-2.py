41fa6abbd0f0f9e27dd44d72c48f53620cad3885
clodoalves <jrjcsj@gmail.com>
2023-04-28 15:10:10 -0300

    Create new samples for object tracking using KCF and CSRT

---
+++ object-tracking/kcf/object-tracking-2.py
@@ -0,0 +1,21 @@
+import cv2
+
+tracker = cv2.TrackerKCF_create()
+video = cv2.VideoCapture("Content/Videos/street.mp4")
+
+isOk, frame = video.read()
+bbox = cv2.selectROI(frame)
+tracker.init(frame, bbox)
+
+while(not(cv2.waitKey(1) & 0XFF == 27) and isOk):
+    isOk, frame = video.read()    
+    isOk, bbox = tracker.update(frame)
+    
+    if (isOk):
+        xaxis, yaxis, width, heigth = bbox[0], bbox[1], bbox[2], bbox[3]
+        cv2.rectangle(frame, (xaxis, yaxis), (xaxis + width, yaxis + heigth), color = (0, 255, 0), thickness=1)
+    else:
+        cv2.putText(frame, "ERROR", org=(155,100), fontFace= cv2.FONT_HERSHEY_SIMPLEX, fontScale= 1, color = (0, 0, 255))
+        
+    cv2.imshow("Sample", frame)
+        
